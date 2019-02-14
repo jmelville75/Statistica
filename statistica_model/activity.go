f23509045c9bfb1f182069c8cd54155b8f6313d3
jmelville75 <37053230+jmelville75@users.noreply.github.com>
2019-02-14 11:23:05 -0500

    Updated

--- statistica_model/activity.go
+++ statistica_model/activity.go
@@ -8,7 +8,7 @@ package statistica_model
 //#define STRING_SET(a,b)   strcpy(a,b)
 //
 //
-//void _BTrees_Prediction_T15_27_0(
+//void _BTrees_Prediction_T12_52_9(
 //      const double * _x__,
 //      const double * _y__,
 //      const double * _z__,
@@ -20417,7 +20417,7 @@ func (a *ModelActivity) Eval(context activity.Context) (done bool, err error) {
 	var i33 = C.double(float64(context.GetInput("_z10__").(float64)))
 
 	var result C.char
-	C._BTrees_Prediction_T15_27_0(&i1, &i2, &i3, &i4, &i5, &i6, &i7, &i8, &i9, &i10, &i11, &i12, &i13, &i14, &i15, &i16, &i17, &i18, &i19, &i20, &i21, &i22, &i23, &i24, &i25, &i26, &i27, &i28, &i29, &i30, &i31, &i32, &i33, &result)
+	C._BTrees_Prediction_T12_52_9(&i1, &i2, &i3, &i4, &i5, &i6, &i7, &i8, &i9, &i10, &i11, &i12, &i13, &i14, &i15, &i16, &i17, &i18, &i19, &i20, &i21, &i22, &i23, &i24, &i25, &i26, &i27, &i28, &i29, &i30, &i31, &i32, &i33, &result)
 
 	context.SetOutput("result", C.GoString(&result))
 
